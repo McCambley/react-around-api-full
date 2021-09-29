2ffbc1037881ad2e4e3d76eb573d7e065a738770
McCambley <jakemccamb16@gmail.com>
2021-09-28 22:45:43 -0400

    Dev: remove password from default query response

--- backend/models/user.js
+++ backend/models/user.js
@@ -39,21 +39,24 @@ const userSchema = new mongoose.Schema({
     required: true,
     type: String,
     minlength: 8,
+    select: false,
   },
 });
 
 userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
-  return this.findOne({ email }).then((user) => {
-    if (!user) {
-      return Promise.reject(new Error('Incorrect email or password'));
-    }
-    return bcrypt.compare(password, user.password).then((matched) => {
-      if (!matched) {
+  return this.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
         return Promise.reject(new Error('Incorrect email or password'));
       }
-      return user;
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error('Incorrect email or password'));
+        }
+        return user;
+      });
     });
-  });
 };
 
 module.exports = mongoose.model('user', userSchema);
