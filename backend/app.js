965aa63a061f6c65b69f947f8b6e0d6d3dfd509c
McCambley <jakemccamb16@gmail.com>
2021-10-03 00:46:37 -0400

    Fix: update port address

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const error = require('./middleware/error');
 const auth = require('./middleware/auth');
 const { requestLogger, errorLogger } = require('./middleware/logger');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/aroundb', {
