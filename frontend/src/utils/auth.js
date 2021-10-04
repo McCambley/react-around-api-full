2dff8a99d6530dbb5de2dea061d056a7a26166f3
McCambley <jakemccamb16@gmail.com>
2021-10-04 10:46:36 -0400

    Dev: configure api urls

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'http://localhost:3000';
+// const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://api.mccambley.students.nomoreparties.site';
 
 export const register = (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
