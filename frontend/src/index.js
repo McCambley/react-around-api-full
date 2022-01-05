11299498fb3a75eef17f9256bc7f5d0b8e005a27
McCambley <jakemccamb16@gmail.com>
2022-01-05 08:02:42 -0500

    Fix: adjust react-router base string

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-around-auth">
+    <BrowserRouter basename="/react-around-api-full">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
